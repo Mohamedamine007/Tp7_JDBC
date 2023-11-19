2175f07f8238369b2074a90ab1782967c410f23e
Mohamed amine <aminebaoui1717@gmail.com>
2023-11-19 18:50:53 +0100

    Add comments to the form.java and DbConnection.java

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -2,48 +2,65 @@ import java.sql.DriverManager;
 import java.sql.*;
 public class DbConnection {
 
+    // Declaring the conn attribute that we'll use a lot using the Connection interface
     private Connection conn;
+    // The url
     private String url = "jdbc:mysql://localhost:3306/bdtest";
 
     public DbConnection() {
 
         try {
+            // Charging the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-
+            //making the connection using the getConnection()
             conn = DriverManager.getConnection(url, "root", "");
         }catch(Exception e) {
-            System.out.println("Fixer a ben 3emi: " + e);
+            System.out.println("Fix your bugs, and errors: " + e);
         }
         }
 
+        // The insert method that we'll call on the "ajouter" button
         public void insert(Person P) throws SQLException{
-
+            // The MySQL query
             String query = "INSERT INTO options VALUES('" + P.nom + "', '" + P.prenom + "', '" + P.age + "')";
+            // creating the statement
             Statement stmt = conn.createStatement();
+            // calling the executeUpdate() method from the stmt object (because we'll modify something)
+            // which is the database table in this case
             stmt.executeUpdate(query);
         }
 
+        // The delete method that we'll call on the "supprimer" button
         public int delete(Person P) throws SQLException {
 
+            // Deletion query
             String query = "DELETE FROM options WHERE nom='"+P.nom+"'";
             Statement stmt = conn.createStatement();
+            // Assigning the value that the executeUpdate() method returns to the rowsAffected variable
             int rowsAffected = stmt.executeUpdate(query);
 
+            // Returning this variable will help us on verifying the success of the deletion
             return rowsAffected;
         }
 
+        // The afficher() method that we'll use inside a table
         public ResultSet afficher() throws SQLException{
 
+        // The selection query
             String query = "SELECT * FROM options";
             Statement stmt = conn.createStatement();
+            // Declaring a ResultSet object, so we can use it to display the data on the table
+            // Because we've a select, this time we use the executeQuery() method.
             ResultSet result = stmt.executeQuery(query);
+            // returning the result object
             return result;
         }
 
+        // the closing connection method
         public void closeConnection() throws SQLException {
-
+            // we've to make sure first if the conn is established, and it's not yet closed before closing it
             if(conn != null && !conn.isClosed()) {
-
+                // The close() method will close the connection (we close the connection, so we stop the program from taking resources
                 conn.close();
                 System.out.println("Connection closed");
             }
--- src/form.java
+++ src/form.java
@@ -6,38 +6,47 @@ import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+//Inheriting from the JFrame class, so we use its method directly
 public class form extends JFrame {
 
+    // Declaring the panels needed to achieve the desired layout in the Tp
     JPanel p1 = new JPanel();
     JPanel p2 = new JPanel();
     JPanel p3 = new JPanel();
     JPanel p4 = new JPanel();
     JPanel mainPanel = new JPanel();
 
+    // Instantiating the buttons from the JButton class
     JButton ajouter = new JButton("Ajouter");
     JButton supprimer = new JButton("Supprimer");
     JButton afficher = new JButton("Afficher");
 
+    // Instantiating the Text Fields that we need
     JTextField nomT = new JTextField();
     JTextField prenomT = new JTextField();
     JTextField ageT = new JTextField();
 
+    // Instantiating from the JOptionPane so that we use it to display explanatory messages
     JOptionPane jOptionPane = new JOptionPane();
 
+    // Instantiating the classes needed to create the table (where we'll display the data)
     DefaultTableModel tableModel;
     JTable table;
 
+    // Declaring a unidimensional array for the table column's name
     String columns[] = {"NOM", "PRENOM", "AGE"};
 
     public form() {
+        // setting the essentials settings for the frame
         setTitle("form");
         setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         setSize(700, 240);
         setLocationRelativeTo(null);
 
+        // Setting the layout
         p1.setLayout(new GridLayout(2, 1));
 
+        //Setting the second panel with its layout components
         p2.setLayout(new GridLayout(3, 2));
         p2.add(new JLabel("nom"));
         p2.add(nomT);
@@ -46,32 +55,47 @@ public class form extends JFrame {
         p2.add(new JLabel("age"));
         p2.add(ageT);
 
+        //Setting the third panel with its layout and components
         p3.setLayout(new FlowLayout());
         p3.add(ajouter);
         p3.add(supprimer);
         p3.add(afficher);
 
+        // Adding an action listener to the "ajouter" button
         ajouter.addActionListener(
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        // Creating a person object so we can use its attributes
                         Person P = new Person();
+                        // Assigning the values gotten from the inputs(textfields) to the person attributes
                         P.nom = nomT.getText();
                         P.prenom = prenomT.getText();
+                        // We convert the age from a string to an integer
                         P.age = Integer.parseInt(ageT.getText());
 
+                        // Instantiating the DbConnection class, so we can interact with the database
                         DbConnection db = new DbConnection();
-
+                        // Because the insert method throws an exception, we've to handel it here
                         try {
+                            //Calling the insert method(to insert data)
                             db.insert(P);
+                            //If data inserted displaying an explanatory message
                             jOptionPane.showMessageDialog(form.this, "Record added successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                            //If we catch an exception
                         } catch (SQLException sqle) {
+                            // displaying a message for the user, you know to make things more interactive
                             jOptionPane.showMessageDialog(form.this, "Record addition FAILED", "Error", JOptionPane.ERROR_MESSAGE);
+                            // Printing the stack trace information to the error output
                             sqle.printStackTrace();
+                            // Adding this finally, so we can execute it however the case
                         } finally {
+                            // Checking if the db object is created successfully(is not null), before start closing
                             if (db != null) {
                                 try {
+                                    // Closing the connection by calling the closeConnection() method
                                     db.closeConnection();
+                                    // because the closeConnection method throws an exception we've to handel it here
                                 } catch (SQLException sqle) {
                                     sqle.printStackTrace();
                                 }
@@ -80,6 +104,10 @@ public class form extends JFrame {
                     }
                 }
         );
+
+        // The same comments that we add for the "ajouter" buttons are almost the same,
+        // so we won't duplicate the comments,
+        // but we'll put a comment if there's something that needs an explanation
 
         supprimer.addActionListener(
                 new ActionListener() {
@@ -91,12 +119,17 @@ public class form extends JFrame {
                         DbConnection db = new DbConnection();
 
                         try {
+                            // The delete method returns an integer that representes the number of rows affected by the query
+                            // so to make sure that the record is deleted successfully we'll use that variable
                             int rowsAffected = db.delete(P);
+                            // Verifying if the rowsAffected > 0(which means that there's some affected rows by the deletion operation)
                             if (rowsAffected > 0) {
+
                                 jOptionPane.showMessageDialog(form.this, "Record deleted successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
                             } else {
                                 jOptionPane.showMessageDialog(form.this, "Record deletion FAILED", "Error", JOptionPane.ERROR_MESSAGE);
                             }
+                            // Catching the exception that the delete method throws
                         } catch (SQLException sqle) {
                             sqle.printStackTrace();
                         } finally {
@@ -118,14 +151,20 @@ public class form extends JFrame {
                     public void actionPerformed(ActionEvent e) {
                         DbConnection db = new DbConnection();
                         try {
+                            // Assigning the data returned by the afficher() method to the selectData variable
+                            // now we can use the selectData variable to display the data
                             ResultSet selectedData = db.afficher();
                             tableModel.setRowCount(0); // Clear existing data in the table
+                            // Start a while loop to loop over the data on the selectedData variable using the next() method
                             while (selectedData.next()) {
+                                // Assigning the values that we get from the selectedData variable
                                 String selectedNom = selectedData.getString("Nom");
                                 String selectedPrenom = selectedData.getString("Prenom");
+                                // Converting the age from the text field from a string to an integer
                                 int selectedAge = selectedData.getInt("Age");
-
+                                //declaring an array of objects so we can store different data types
                                 Object[] rowData = {selectedNom, selectedPrenom, selectedAge};
+                                // Adding that data as a row to the tableModel
                                 tableModel.addRow(rowData);
                             }
                         } catch (SQLException sqle) {
@@ -135,6 +174,7 @@ public class form extends JFrame {
                 }
         );
 
+        // Create an initially empty table model with column names
         tableModel = new DefaultTableModel(null, columns);
         table = new JTable(tableModel);
         table.setShowGrid(true);
